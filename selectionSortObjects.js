c3948ea7fe61fc405dbcabef01845a1f55230d18
Fernando Gomez <fernandogomez37@yahoo.com>
2015-09-30 20:18:53 -0700

    object sort

--- selectionSortObjects.js
+++ selectionSortObjects.js
@@ -12,3 +12,28 @@ var persons = [
   "age": "25"
   }
 ];
+
+function selectionSortObjects (arr){
+  var length = arr.length;
+  for(var i = 0; i < length; i++){
+    var min = [i].age;
+    for(var j = i+1; j < length; j++) {
+      if(arr [j].age > arr[min]){
+        min = [j].age;
+      }
+    }
+    if (min != i.age) {
+      var k = arr[i].age;
+      arr[i].age = arr[min];
+      arr[min] = k;
+    }
+  }
+  return arr;
+}
+
+console.log(selectionSortObjects(persons));
+console.log(persons[0].age);
+// persons.sort(function(a,b){
+//   return a.age-b.age;
+// })
+// console.log(persons);
